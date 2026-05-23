21c1f83edd88476e7606d91bcecec62555f48c2f
Simar Preet Singh <simarpreet.singh.ece21@itbhu.ac.in>
2026-05-24 02:04:38 +0530

    Add the hooks for various events in websocket

--- experiments/websocket-simple-echo-server/src/main/java/com/learning/App.java
+++ experiments/websocket-simple-echo-server/src/main/java/com/learning/App.java
@@ -9,5 +9,22 @@ public class App {
         app.get("/", ctx -> ctx.result("Server is Up!") );
 
         System.out.println("Hello JAVALIN BITCH");
+
+        app.ws("/echo", ws -> {
+           ws.onConnect(ctx -> {
+            System.out.println("Client Connected Yay!");
+           }); //I need to implement constant listening here
+           //Update These are event handlers, constant listening is already being
+           //done under the hood
+
+           ws.onMessage(ctx -> {
+            String message = ctx.message();
+            System.out.println("Message Received");
+            ctx.send("Returning Message " + message);
+           }); //Return the same message
+
+
+           ws.onClose(ctx -> System.out.println("Closed!")); //Well Stop the constant listening simply
+        });
     }
 }
